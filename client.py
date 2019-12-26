e2f8fb05fc4499a6cc82c9a1f6a60fa9d006e469
Denis <123cryisfree@mail.ru>
2019-12-26 13:54:37 +0300

    main task`s done

--- client.py
+++ client.py
@@ -1,14 +1,44 @@
 import socket
 import pickle
+from random import randint
 
-HOST = '127.0.0.1'
-PORT = 8080
+def encrypt(m, k):
+    return ''.join([chr((ord(x) + k) % 65536) for x in m])
+
+def decrypt(m, k):
+    return ''.join([chr((ord(x) - k) % 65536) for x in m])
+
+HOST = 'localhost'
+
+try:
+    PORT=int(input("PORT:"))
+    if not 0 <= PORT <= 65535:
+        raise ValueError
+except ValueError :
+    PORT = int(input('Something went wrong, try again:'))
 
 sock = socket.socket()
 sock.connect((HOST, PORT))
 
-p, g, a = 7, 5, 3
+p, g, a = randint(1, 1000), randint(1, 1000), randint(1, 1000)
+
 A = g ** a % p
+
 sock.send(pickle.dumps((p, g, A)))
+
+
+
+while True:
+    tt = input('Message: ')
+    if tt == "":
+        break
+    
+    sock.send(pickle.dumps(tt))
+    msg = sock.recv(1024)
+    B = pickle.loads(msg)[0]
+    msg = pickle.loads(msg)[1]
+    print(msg)
+    k = B ** a % p
+    print(decrypt(msg, k))
 
 sock.close()
